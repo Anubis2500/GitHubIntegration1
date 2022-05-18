208049d66ff4a3a470c264705ac018c223f8cac3
Anubis2500 <104906377+Anubis2500@users.noreply.github.com>
2022-05-18 17:46:32 +0200

    new repo

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Second line of source code!");
+
+        System.out.println("third line");
+        System.out.println("fourth line");
+    }
+}
